d0c08a82fe9cdb2cedf583e837e32449631bd53e
中村 <n.kosuko731@gmail.com>
2025-04-06 02:48:26 +0900

    ユーザー情報設定画面への遷移設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
+
   root to: "top#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
