563e7d42c3a3c243bb8a0aed1797b471f33cce41
中村 <n.kosuko731@gmail.com>
2025-04-08 01:52:16 +0900

    ユーザーネーム変更機能の実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,8 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  # updateメソッド時に与えるストロングパラメータの設定用。
+  before_action :configure_account_update_params, only: [ :update ]
 
   # GET /resource/sign_up
   # def new
@@ -38,17 +39,29 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
+  # password無しで詳細を変更するための記載。
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
+
+  # updateメソッド実行時の遷移先指定用オーバーライド
+  # 更新が終わった際に、再度プロフィール設定画面が表示されるようにしたい
+  def after_update_path_for(resource)
+    edit_user_registration_path(resource) # ここにリダイレクト先のパスを指定
+  end
+
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
