2f0254f9af795dc1fc4ae52809d3730ed529a21a
中村 <n.kosuko731@gmail.com>
2025-04-23 18:14:19 +0900

    feat:パレットいいね機能の非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,20 +2,26 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = current_user.likes.new(post_id: @post.id)
-    if @like.save
-      redirect_to request.referer, notice: "パレットをいいねしました"
-    else
-      redirect_to request.referer, alert: "不具合により、パレットをいいねできませんでした"
+    respond_to do |format|
+      if @like.save
+        format.turbo_stream
+        format.html { redirect_to request.referer, notice: "パレットをいいねしました" }
+      else
+        format.html { redirect_to request.referer, alert: "不具合により、パレットをいいねできませんでした" }
+      end
     end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @like = current_user.likes.find_by(post_id: @post.id)
-    if @like.destroy
-      redirect_to request.referer, notice: "パレットのいいねを外しました"
-    else
-      redirect_to request.referer, alert: "不具合により、パレットのいいねを外せませんでした"
+    respond_to do |format|
+      if @like.destroy
+        format.turbo_stream
+        format.html { redirect_to request.referer, notice: "パレットのいいねを外しました" }
+      else
+        format.html { redirect_to request.referer, alert: "不具合により、パレットのいいねを外せませんでした" }
+      end
     end
   end
 end
