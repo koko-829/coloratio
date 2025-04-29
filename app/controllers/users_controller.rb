0ab0436ce8ae6253550a7bbf3a3289c499ca6123
中村 <n.kosuko731@gmail.com>
2025-04-29 18:20:59 +0900

    feat:他ユーザーのプロフィール画面大枠作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,16 @@ class UsersController < ApplicationController
       return
     end
 
-    published_posts = Post.where(user_id: @user.id, status: "published").order(created_at: :desc)
-    @my_page_published_posts = published_posts.limit(5)
-    @published_count = published_posts.count
-    draft_posts = Post.where(user_id: @user.id, status: "draft").order(created_at: :desc)
-    @my_page_draft_posts = draft_posts.limit(5)
-    @draft_count = draft_posts.count
+    if @user.id == current_user.id # 自分のアカウントだった場合
+      published_posts = Post.where(user_id: @user.id, status: "published").order(created_at: :desc)
+      @my_page_published_posts = published_posts.limit(5)
+      @published_count = published_posts.count
+      draft_posts = Post.where(user_id: @user.id, status: "draft").order(created_at: :desc)
+      @my_page_draft_posts = draft_posts.limit(5)
+      @draft_count = draft_posts.count
+    else # 他のユーザーだった場合
+      published_posts = Post.where(user_id: @user.id, status: "published").order(created_at: :desc)
+      @pagy, @published_posts  = pagy(published_posts)
+    end
   end
 end
