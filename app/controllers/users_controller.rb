66366a0c0b4b37ce6b25c9e2a758ef981ab4a2aa
中村 <n.kosuko731@gmail.com>
2025-05-03 05:22:32 +0900

    feat:マイページの各パレット表示タブ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,32 @@ class UsersController < ApplicationController
     end
 
     if @user.id == current_user.id # 自分のアカウントだった場合
-      published_posts = Post.where(user_id: @user.id, status: "published").order(created_at: :desc)
-      @my_page_published_posts = published_posts.limit(5)
+      published_posts = Post.where(user_id: @user.id, status: "published").latest
+      draft_posts = Post.where(user_id: @user.id, status: "draft").latest
+      liked_posts = @user.liked_posts
+
+      # 各パレット数取得
       @published_count = published_posts.count
-      draft_posts = Post.where(user_id: @user.id, status: "draft").order(created_at: :desc)
-      @my_page_draft_posts = draft_posts.limit(5)
       @draft_count = draft_posts.count
+      @liked_count = liked_posts.count
+
+      # 各パレット表示用
+      if params[:published]
+        @pagy, @my_published_posts = pagy(published_posts, limit: 4)
+        @selected_tab = "published"
+      elsif params[:draft]
+        @pagy, @draft_posts = pagy(draft_posts, limit: 4)
+        @selected_tab = "draft"
+      elsif params[:liked]
+        @pagy, @liked_posts = pagy(liked_posts, limit: 4)
+        @selected_tab = "liked"
+      else
+        @pagy, @my_published_posts = pagy(published_posts, limit: 4)
+        @selected_tab = "published"
+      end
+
     else # 他のユーザーだった場合
-      published_posts = Post.where(user_id: @user.id, status: "published").order(created_at: :desc)
+      published_posts = Post.where(user_id: @user.id, status: "published").latest
       @pagy, @published_posts  = pagy(published_posts)
     end
   end
