9ab5ff18e05731bb4f81043329700e06e2c01e67
中村 <n.kosuko731@gmail.com>
2025-05-10 14:16:18 +0900

    Revert "Feature/add maintenance mode"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :check_maintenance_mode
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include Pagy::Backend
@@ -9,15 +8,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
-  end
-
-  private
-  # メンテナンスモードにするための記載。
-  # 環境変数'MAINTENANCE_MODE'が1の場合は/app/views/maintenance/maintenance.html.erbをレンダリングする
-  # layout: nilにすることでヘッダーとかは表示しない感じにする。
-  def check_maintenance_mode
-    if ENV["MAINTENANCE_MODE"] == "1"
-      render template: "maintenance/maintenance", status: :service_unavailable, layout: nil
-    end
   end
 end
