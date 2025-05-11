0c07e2d9a7f72756f771ece6f88f92043a2d206c
中村 <n.kosuko731@gmail.com>
2025-05-11 20:50:48 +0900

    feat:ユーザーネームのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :user_colors
   has_many :colors, through: :user_colors
   validates :profile_icon, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 8 }
+  validates :name, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
