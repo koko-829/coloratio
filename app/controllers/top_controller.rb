c8bdc7fd12d8596eafef1f16854d426b3c806323
中村 <n.kosuko731@gmail.com>
2025-05-12 16:48:33 +0900

    feat:ログイン前トップページの微修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
     # published、latestともにモデルでscope定義済み
-    @posts = Post.joins(:user).where(users: { name: "oloratio" }).published.latest.limit(10)
+    @posts = Post.joins(:user).where(users: { name: "Coloratio" }).published.latest.limit(10)
   end
 end
