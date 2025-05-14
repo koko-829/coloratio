1e89b3bb00603987a224016fa9aea50fae37d310
中村 <n.kosuko731@gmail.com>
2025-05-14 18:29:00 +0900

    feat:ログイン前ヘッダーのボタンサイズ変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   has_many :color_posts, dependent: :destroy
   has_many :colors, through: :color_posts
   has_many :color_suggestions
-  has_many :tag_posts
+  has_many :tag_posts, dependent: :destroy
   has_many :tags, through: :tag_posts
   enum status: { draft: 0, published: 1 }
   validates :title, length: { maximum: 32 }
