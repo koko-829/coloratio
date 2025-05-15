ff257a0c40200271a8dbd3fb198ea9a6a52885cc
中村 <n.kosuko731@gmail.com>
2025-05-16 05:11:37 +0900

    fix:ログイン前トップページがログイン状態で表示されないように修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,10 @@
 class TopController < ApplicationController
   def index
+    # ログイン状態で「ログイン前トップページ」を表示できないようにするための処理。
+    if user_signed_in?
+      redirect_to :root
+      return
+    end
     # published、latestともにモデルでscope定義済み
     @posts = Post.joins(:user).where(users: { name: "Coloratio" }).published.latest.limit(10)
   end
