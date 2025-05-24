da14715b1858fdda0a6091d9c720a8002a9ba39e
中村 <n.kosuko731@gmail.com>
2025-05-24 09:40:41 +0900

    feat:パレット検索機能の実装

--- app/models/color.rb
+++ app/models/color.rb
@@ -3,4 +3,8 @@ class Color < ApplicationRecord
   has_many :posts, through: :color_posts
   has_many :user_colors
   has_many :users, through: :user_colors
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "hex_code" ]
+  end
 end
