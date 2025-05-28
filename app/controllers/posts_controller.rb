4cc7a4dcb80f57b6039fb2c80e2758edebea389e
中村 <n.kosuko731@gmail.com>
2025-05-28 12:24:56 +0900

    feat:turbo_streamを使用したパレット詳細画面の表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -125,6 +125,10 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
   end
 
   def destroy
