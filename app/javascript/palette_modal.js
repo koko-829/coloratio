bbf92b878abc473ef05b52db1fb766b8df8deb41
中村 <n.kosuko731@gmail.com>
2025-05-30 03:48:37 +0900

    feat:パレット詳細モーダルとトップスクロールボタンのz-index調整

--- app/javascript/palette_modal.js
+++ app/javascript/palette_modal.js
@@ -1,10 +1,9 @@
 // パレット詳細画面のスケルトンローディング用のjs
+
 // パレット詳細モーダル開く瞬間にスケルトンローディングを表示したい。
 document.addEventListener("turbo:before-fetch-request", function() {
-  // document.querySelector(".modal-palette").style.display = "none";
   const skelton = document.getElementById("palette-skelton");
   if (skelton) {
-    console.log('skeltonを見つけました。表示します。');
     skelton.style.display = "block";
   }
 });
@@ -12,14 +11,10 @@ document.addEventListener("turbo:before-fetch-request", function() {
 // すべて読み込んだらスケルトンを非表示にしたい。
 document.addEventListener("turbo:before-stream-render", function() {
   const skelton = document.getElementById("palette-skelton");
-  const modal = document.querySelector(".modal-palette");
   // 数秒後にスケルトンを非表示にする
-  if (skelton && modal) {
-    console.log('skeltonとmodalを見つけました。数秒後にmodalを表示します。');
+  if (skelton) {
     setTimeout(function() {
-      document.querySelector(".modal-palette").style.display = "block";
       skelton.style.display = "none"; // スケルトンを非表示
     }, 500);
-
   }
 });
