2a7ca4f56da2bcf5def2890de9294ebe03eb27a1
中村 <n.kosuko731@gmail.com>
2025-06-07 03:51:08 +0900

    feat:xシェアボタンの実装

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,11 +19,17 @@ module.exports = {
       },
       animation: {
         'slide-top': 'slide-top 0.7s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'fuwafuwa': 'fuwafuwa 3s ease-in-out infinite alternate'
       },
       keyframes: {
         'slide-top': {
           '0%': { transform: 'translateY(50px)' },
           'to': { transform: 'translateY(0px)' },
+        },
+        'fuwafuwa' : {
+          '0%': {transform: 'translate(0, 0)' },
+          '50%': {transform: 'translate(0, -10px)' },
+          '100%': {transform: 'translate(0, 0)' },
         },
       },
     },
@@ -35,3 +41,5 @@ module.exports = {
   ],
   darkMode: 'class'
 }
+
+
