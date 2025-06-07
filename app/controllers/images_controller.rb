335ee401c4686f66c3335797d882932e4fe0fa7d
中村 <n.kosuko731@gmail.com>
2025-06-07 20:36:30 +0900

    feat:clouditionaryの導入

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,8 @@
 class ImagesController < ApplicationController
+  # skip_before_action :require_login, raise: false
+
+  def ogp
+    base64_image = params[:image]  # html2canvasから送られたBase64データを受け取る
+    send_data base64_image, type: "image/png", disposition: "inline"  # 画像データを直接返す
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,9 @@ Rails.application.routes.draw do
     end
   end
 
+  # images/ogp用のアクションルーティング
+  get "images/ogp.png", to: "images#ogp", as: "images_ogp"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
