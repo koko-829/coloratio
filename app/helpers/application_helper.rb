b82efc18f2e863a81fd1d55afb5956d6b39b0775
中村 <n.kosuko731@gmail.com>
2025-06-10 04:34:56 +0900

    feat:デプロイ先確認用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def default_meta_tags
-    if controller_name == "posts" && action_name == "show"
+    if controller_name == "posts" && action_name == "index"
       # ここでポストのメタタグを設定する
       {
         site: "Coloratio",
@@ -60,7 +60,7 @@ module ApplicationHelper
 
   def posts_twitter_card
     {
-      title: @post.title,
+      title: "post投稿"
       description: "ポスト投稿用のOG",
       card: "summary_large_image",
       # image: @post.ogp_url.presence || image_url("ogp.png")
