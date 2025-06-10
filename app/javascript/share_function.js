6dd6fa45b781ee179ba1d890a6b29590fa7e1dcd
中村 <n.kosuko731@gmail.com>
2025-06-10 21:50:40 +0900

    feat:ポップアップブロックの対策

--- app/javascript/share_function.js
+++ app/javascript/share_function.js
@@ -2,9 +2,15 @@ document.addEventListener("turbo:before-stream-render", function() {
   setTimeout(function() {
     const shareTrigger = document.getElementById("share-trigger");
     if (shareTrigger) {
-      // shareTrigger.click();
-      window.open(twitterShareLink.href, '_blank');
-      shareTrigger.remove();
+      const shareLink = shareTrigger.href; // リンクのURLを取得
+      // 新しいウィンドウを開く
+      const newWindow = window.open(shareLink, '_blank');
+      // ポップアップがブロックされたかチェック
+      if (!newWindow) {
+        // ポップアップがブロックされた場合、location.hrefで遷移
+        location.href = shareLink;
+      }
+      shareTrigger.remove(); // 要素を削除
     }
   }, 300);
 });
