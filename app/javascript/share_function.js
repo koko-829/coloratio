94bc896445d06c9baa2c1b45a6fa59a2908eda40
中村 <n.kosuko731@gmail.com>
2025-06-14 08:23:02 +0900

    feat:xシェアボタンクリック時のポップアップブロック対策

--- app/javascript/share_function.js
+++ app/javascript/share_function.js
@@ -3,6 +3,11 @@ document.addEventListener("turbo:before-stream-render", function() {
     const shareTrigger = document.getElementById("share-trigger");
     if (shareTrigger) {
       const shareLink = shareTrigger.href; // リンクのURLを取得
+      const popupBlockerMeasure = document.getElementById('popup-blocker-measure');
+      // ポップアップブロック対策として空要素をクリックさせておく。
+      if (popupBlockerMeasure) {
+        popupBlockerMeasure.click();
+      }
       // 新しいウィンドウを開く
       const newWindow = window.open(shareLink, '_blank');
       // ポップアップがブロックされたかチェック
@@ -10,6 +15,7 @@ document.addEventListener("turbo:before-stream-render", function() {
         // ポップアップがブロックされた場合、location.hrefで遷移
         location.href = shareLink;
       }
+      popupBlockerMeasure.remove();
       shareTrigger.remove(); // 要素を削除
     }
   }, 300);
