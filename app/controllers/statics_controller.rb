b56423fec5f46131d269ef899cec158558899b9a
中村 <n.kosuko731@gmail.com>
2025-06-14 01:21:41 +0900

    feat:フラッシュメッセージの表示位置調整

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -13,17 +13,4 @@ class StaticsController < ApplicationController
 
   def practice
   end
-
-  def colors_update
-    post_id = params[:post_id]
-    @post = Post.find_by(id: post_id)
-    if @post
-      @colors = @post.colors.pluck(:hex_code)
-      @post.update(color_list: @colors.join(","))
-      flash[:notice] = "#{@post.color_list}"
-    else
-      flash[:alert] = "#{post_id}のpostが見つかりませんでした"
-    end
-    redirect_to practice_static_path
-  end
 end
