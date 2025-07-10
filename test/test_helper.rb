bb934f9e4dd0b450513ce535abcb560e997767e1
中村 <n.kosuko731@gmail.com>
2025-07-11 01:38:29 +0900

    feat:test_helperにsimplecovの追記

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,22 @@
+# test/test_helper.rb
+
+# 忘れずに追加
+require "simplecov"
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+# テスト結果見やすくするためのプラグイン
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require "minitest/autorun"
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    # parallelizeはSimpleCovと互換性が悪いため、一旦コメントアウトする
+    # parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
