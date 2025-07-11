e80dff1ccc1adc91e3ee98f54266da4f19b88ee7
中村 <n.kosuko731@gmail.com>
2025-07-12 00:13:19 +0900

    feat:profile_icons.ymlファイルの削除(不要fixtures)

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  # test "should get new" do
+  #   get new_post_path
+  #   assert_response :success
   # end
+
+  test "check my hat" do
+		assert true
+	end
 end
