3f862e27bac0df6a8e36e38b651ea46502c8497d
中村 <n.kosuko731@gmail.com>
2025-07-12 01:49:37 +0900

    test:テストコード初期化

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,12 +1,4 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get new" do
-  #   get new_post_path
-  #   assert_response :success
-  # end
-
-  test "check my hat" do
-		assert true
-	end
 end
