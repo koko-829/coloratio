cfd2c4c8380f06fc1660ae7a65e24e8d61812d22
中村 <n.kosuko731@gmail.com>
2025-07-28 12:52:44 +0900

    docs:CI/CDのテスト自動化設定箇所コメントアウト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,15 @@ class UserTest < ActiveSupport::TestCase
     user = users(:taro)
     assert user.valid?
   end
+
+  # emailが空の場合に無効となるか？
+  test "invalid without email" do
+    user = User.new(email: nil)
+    assert_not user.valid?
+  end
+
+  # encrypted_passwordが存在しない場合に無効となるか？
+  # 既存メールアドレスでユーザー作成しようとした際にエラーが起こるかどうか？
+  # 同じ名前でユーザー作成ができないようになっているかどうか?
+  # profile_iconのバリデーションを超える整数の場合に無効となるか？
 end
