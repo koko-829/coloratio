a7b53b1ad3b1410ce96ae91d9a1e227853c73241
中村 <n.kosuko731@gmail.com>
2025-07-29 12:34:02 +0900

    test:既存ユーザーの有効性確認テストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  # 既存のユーザーが有効であることを確認するテスト
   test "valid user" do
     user = users(:taro)
+    assert user.valid?
+  end
+
+  # 名前、メールアドレス、パスワードが正しい場合にユーザーが作成できることを確認するテスト
+  test "valid with name, email, and password" do
+    user = User.new(
+      name: "sample",
+      password: "sample123",
+      email: "sample@email.com"
+    )
     assert user.valid?
   end
 
